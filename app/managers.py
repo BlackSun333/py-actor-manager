3e0925879c635cc51d3964fd3b7b3ea7265605e7
Stanislav <staskobetc@gmail.com>
2026-04-27 17:24:08 +0300

    Solution

--- app/managers.py
+++ app/managers.py
@@ -4,7 +4,7 @@ from app.models import Actor
 
 
 class ActorManager:
-    def __init__(self, db_name: str, table_name: str):
+    def __init__(self, db_name: str, table_name: str) -> None:
         self.db_name = db_name
         self.table_name = table_name
         self.connection = sqlite3.connect(self.db_name)
@@ -22,7 +22,9 @@ class ActorManager:
         self.connection.commit()
 
     def create(self, first_name: str, last_name: str) -> None:
-        query = f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)"
+        query = (f"INSERT INTO "
+                 f"{self.table_name}"
+                 f" (first_name, last_name) VALUES (?, ?)")
         self.connection.execute(query, (first_name, last_name))
         self.connection.commit()
 
@@ -31,7 +33,9 @@ class ActorManager:
         cursor = self.connection.cursor()
         cursor.execute(query)
         rows = cursor.fetchall()
-        return [Actor(id=row[0], first_name=row[1], last_name=row[2]) for row in rows]
+        return [Actor(id=row[0],
+                      first_name=row[1],
+                      last_name=row[2]) for row in rows]
 
     def update(self, pk: int, new_first_name: str, new_last_name: str) -> None:
         query = f"""
